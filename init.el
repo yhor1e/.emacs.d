030eb03c32a6afbf4f26d863c41c9d854e4d30ca
yhorie <yuk.horie@gmail.com>
2016-12-23 00:48:30 +0900

    Refactor

--- init.el
+++ init.el
@@ -1,150 +1,281 @@
-(defvar *installp* nil)
+;;; Package --- SUMMARY:
+;;; Code:
+;;; Commentary:
+;;;    initialize: install package use-package
 
 (add-to-list 'load-path "~/.emacs.d/elisp/")
 
 (require 'package)
-(setq package-check-signature nil)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(setq package-archives
+  '(("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.org/packages/")
+     ("org" . "http://orgmode.org/elpa/")))
+
 (package-initialize)
 
-(when (and *installp* (require 'package nil 'noerror))
+;; nil -> 1 to install packages
+(if nil
+
   (package-refresh-contents)
 
-  (defvar dependencies
+  (defvar init-dependencies
     '(
-      ;;     init-loader
-      exec-path-from-shell
+       exec-path-from-shell
 
-      redo+
-      undohist
-      undo-tree
+       redo+
 
-      mozc
+       mozc
 
-      popwin
-      direx
+       popwin
+       direx
 
-      auto-complete
-      fuzzy
+       auto-complete
+       fuzzy
 
-      web-mode
-      emmet-mode
-      js2-mode
-      json-mode
-      markdown-mode
-      flycheck
+       web-mode
+       emmet-mode
+       js2-mode
+       json-mode
+       markdown-mode
+       flycheck
 
-      editorconfig
+       editorconfig
 
-      yasnippet
+       yasnippet
 
-      anzu
-      volatile-highlights
-      hlinum
-      ))
+       anzu
+       volatile-highlights
+       hlinum
+       ))
 
-  (dolist (package dependencies)
+  (dolist (package init-dependencies)
     (unless (package-installed-p package)
       (package-install package))))
 
-(exec-path-from-shell-initialize)
 
-;; set encode
+;;; set key
+;; C-h
+(keyboard-translate ?\C-h ?\C-?)
+;; C-x ?
+(global-set-key (kbd "C-x ?") 'help-command)
+;; C-z
+(global-set-key "\C-z" 'undo)
+;; C-t
+(define-key global-map (kbd "C-t") 'other-window)
+
+
+;;; disabled auto-save
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+
+;;; startup screen
+(setq inhibit-startup-screen t)
+
+
+;;; minibuffer
+(setq frame-title-format
+  (format "%%f"))
+
+
+;;; line number
+(global-linum-mode)
+(use-package hlinum
+  :config
+  (hlinum-activate))
+
+
+;;; encode
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
 (setq file-name-coding-system 'utf-8)
 (setq locale-coding-system 'utf-8)
 
-;; set font
+
+;;; font
 (set-face-attribute 'default nil
-		    :family "RictyDiminished"
-		    :height 120)
+  :family "RictyDiminished"
+  :height 120)
 
-;; set display
-(setq frame-title-format
-      (format "%%f"))
 
-(global-linum-mode)
+;;; input method
+(use-package mozc
+  :config
+  (setq default-input-method "japanese-mozc")
+  (setq mozc-candidate-style 'echo-area)
+  (global-set-key (kbd "S-SPC") 'toggle-input-method)
+  (define-key mozc-mode-map (kbd "S-SPC") 'toggle-input-method)
+  (add-hook 'input-method-activate-hook (lambda () (set-face-background 'cursor "#3a3")))
+  (add-hook 'input-method-inactivate-hook (lambda () (set-face-background 'cursor "#666"))))
 
-(setq inhibit-startup-screen t)
 
+;;; hide tool bar
 (tool-bar-mode 0)
 
+
+;;; spaces, tabs
+;; disable whitespace
 (global-whitespace-mode 0)
+;; trailing space
+(setq-default show-trailing-whitespace 0)
+(set-face-background 'trailing-whitespace "#b14770")
+;; tab
+(use-package whitespace
+  :config
+  (setq whitespace-style '(tabs tab-mark))
+  (global-whitespace-mode 1))
 
-(setq make-backup-files nil)
-(setq auto-save-default nil)
 
-;; disabled auto-save
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
-;; auto revert buffer
+;;; auto revert buffer
 (global-auto-revert-mode 1)
 
-;; paran-mode
+
+;;; redo+
+;; http://www.emacswiki.org/emacs/download/redo+.el
+(use-package redo+
+  :config
+  (global-set-key (kbd "C-S-z") 'redo))
+
+
+;;; paran-mode
 (setq show-paren-delay 0)
 (show-paren-mode t)
 (setq show-paren-style 'expression)
 (set-face-background 'show-paren-match-face "#424242")
 
-;; set auto-install
-;; http://www.emacswiki.org/emacs/download/auto-install.el
-(when (require 'auto-install nil t)
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-  (auto-install-update-emacswiki-package-name t)
-  (auto-install-compatibility-setup))
 
-;; set redo+
-;; http://www.emacswiki.org/emacs/download/redo+.el
-(when (require 'redo+ nil t)
-  (global-set-key (kbd "C-S-z") 'redo))
+;;; theme/color
+(load-theme 'wombat t)
+(set-face-background 'region "#226666")
 
-;; C-h
-(keyboard-translate ?\C-h ?\C-?)
-(global-set-key (kbd "C-x ?") 'help-command)
 
-;; C-z
-(global-set-key "\C-z" 'undo)
+;;; ido-mode
+(ido-mode t)
+(use-package ido
+  :config
+  (setq ido-everywhere t)
+  (setq ido-enable-flex-matching t)
+  (setq ido-create-new-buffer 'always))
+(when (boundp 'confirm-nonexistent-file-or-buffer)
+  (setq confirm-nonexistent-file-or-buffer nil))
+(global-set-key (kbd "C-x f") 'ido-find-file-other-window)
 
-;; C-t
-(define-key global-map (kbd "C-t") 'other-window)
 
-;; for html etc
-(when (require 'web-mode nil t)
+;;; html
+;; browser
+(setq browse-url-browser-function 'browse-url-generic
+  browse-url-generic-program "google-chrome")
+;; web-mode
+(use-package web-mode
+  :config
   (add-to-list 'auto-mode-alist '("\\.html?$" . web-mode))
   (define-key web-mode-map (kbd "C-c C-v") 'browse-url-of-buffer))
 
-(setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "google-chrome")
 
-;; for css
-(autoload 'css-mode "css-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
-(add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
-(add-to-list 'auto-mode-alist '("\\.scss$" . css-mode))
+;;; css
+(use-package css-mode
+  :commands css-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
+  (add-to-list 'auto-mode-alist '("\\.less$" . css-mode))
+  (add-to-list 'auto-mode-alist '("\\.scss$" . css-mode)))
 
-;; for JavaScript
+
+;;; JavaScript
 ;; js2-mode
-(autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.es$" . js2-mode))
-(add-hook 'js2-mode-hook 'js-indent-hook)
-(setq js2-strict-missing-semi-warning nil)
+(use-package js2-mode
+  :commands js2-mode
+  :init
+  ;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+  (add-to-list 'auto-mode-alist '("\\.js$" . js2-jsx-mode))
+  ;;  (add-hook 'js2-mode-hook 'js-indent-hook)
+  ;;  (setq js2-strict-missing-semi-warning nil)
+  )
 
-;; flycheck
+
+;;; flycheck
 (global-flycheck-mode)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (setq-default flycheck-disabled-
   (append flycheck-disabled-checkers
     '(javascript-jshint)))
-(flycheck-add-mode 'html-tidy  'web-mode)
-(flycheck-add-mode 'javascript-eslint 'js2-mode)
+(flycheck-add-mode 'html-tidy 'web-mode)
+(flycheck-add-mode 'css-csslint 'css-mode)
+;; (flycheck-add-mode 'javascript-eslint 'js2-mode)
+(flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
+
+;;; cua-mode
+(cua-mode t)
+(setq cua-enable-cua-keys nil)
 
 
-;; auto-complete
-(when (require 'auto-complete-config nil t)
+(use-package popwin
+  :config
+  (popwin-mode 1))
+
+(use-package direx
+  :config
+  (push '(direx:direx-mode :position left :width 50 :dedicated t)
+    popwin:special-display-config)
+  (global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window))
+
+(use-package anzu
+  :config
+  (global-anzu-mode +1)
+  (custom-set-variables
+    '(anzu-mode-lighter "")
+    '(anzu-deactivate-region t)
+    '(anzu-search-threshold 1000)))
+
+(use-package volatile-highlights
+  :config
+  (volatile-highlights-mode t))
+
+;;; markdown-mode
+(use-package markdown-mode
+  :commands markdown-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode)))
+
+;;; emmet-mode
+(use-package emmet-mode
+  :config
+  (add-hook 'web-mode-hook 'emmet-mode)
+  (setq emmet-move-cursor-between-quotes t))
+
+;;; yasnippet
+(use-package yasnippet
+  :config
+  (setq yas-snippet-dirs
+    '("~/.emacs.d/snippets"))
+  (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+  (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+  (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+  (yas-global-mode 1))
+
+;;; editorconfig
+(use-package editorconfig
+  :ensure t
+  :config
+  (setq editorconfig-get-properties-function
+    'editorconfig-core-get-properties-hash)
+  (editorconfig-mode 1))
+
+
+(exec-path-from-shell-initialize)
+
+;;; set auto-install
+;; http://www.emacswiki.org/emacs/download/auto-install.el
+(use-package auto-install
+  :config
+  (setq auto-install-directory "~/.emacs.d/elisp/")
+  (auto-install-update-emacswiki-package-name t)
+  (auto-install-compatibility-setup))
+
+;;; auto-complete
+(use-package auto-complete-config
+  :config
   (add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-20150618.1949/dict")
   (ac-config-default)
   (setq ac-use-fuzzy t)
@@ -173,95 +304,7 @@
   (add-hook 'js2-mode-hook 'ac-js2-mode-setup)
   )
 
-;; editorconfig
-					;(load "editorconfig")
+;;(add-hook emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 
-(when (locate-library "editorconfig")
-  (editorconfig-mode 1)
-  )
-
-
-;; cua-mode
-(cua-mode t)
-(setq cua-enable-cua-keys nil)
-
-;; yasnippet
-(when (require 'yasnippet nil t)
-  (setq yas-snippet-dirs
-	'("~/.emacs.d/snippets"))
-  (define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
-  (define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
-  (define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
-  (yas-global-mode 1))
-
-;; markdown-mode
-(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-
-;; emmet-mode
-(when (require 'emmet-mode nil t)
-  (add-hook 'web-mode-hook 'emmet-mode)
-  (setq emmet-move-cursor-between-quotes t))
-
-;; theme/color
-(load-theme 'wombat t)
-(set-face-background 'region "#226666")
-
-;; trailing space
-(setq-default show-trailing-whitespace 0)
-(set-face-background 'trailing-whitespace "#b14770")
-
-;; display tab
-(when (require 'whitespace nil t)
-  (setq whitespace-style '(tabs tab-mark))
-  (global-whitespace-mode 1))
-
-;; mozc
-(when (require 'mozc nil t)
-  (setq default-input-method "japanese-mozc")
-  (setq mozc-candidate-style 'echo-area)
-  (global-set-key (kbd "S-SPC") 'toggle-input-method)
-  (define-key mozc-mode-map (kbd "S-SPC") 'toggle-input-method)
-  (add-hook 'input-method-activate-hook (lambda () (set-face-background 'cursor "#3a3")))
-  (add-hook 'input-method-inactivate-hook (lambda () (set-face-background 'cursor "#666"))))
-
-(when (require 'undohist nil t)
-  (undohist-initialize))
-
-(when (require 'popwin nil t)
-  (popwin-mode 1))
-
-(when (require 'direx nil t)
-  (push '(direx:direx-mode :position left :width 50 :dedicated t)
-	popwin:special-display-config)
-  (global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window))
-
-(when (require 'undo-tree nil t)
-  (global-undo-tree-mode))
-
-(when (require 'point-undo nil t)
-  (define-key global-map (kbd "M-[") 'point-undo)
-  (define-key global-map (kbd "M-]") 'point-redo))
-
-(when (require 'anzu nil t)
-  (global-anzu-mode +1)
-  (custom-set-variables
-   '(anzu-mode-lighter "")
-   '(anzu-deactivate-region t)
-   '(anzu-search-threshold 1000)))
-
-(when (require 'volatile-highlights nil t)
-  (volatile-highlights-mode t))
-
-(ido-mode t)
-(require 'ido)
-(setq ido-everywhere t)
-(setq ido-enable-flex-matching t)
-(setq ido-create-new-buffer 'always)
-(when (boundp 'confirm-nonexistent-file-or-buffer)
-  (setq confirm-nonexistent-file-or-buffer nil))
-(global-set-key (kbd "C-x f") 'ido-find-file-other-window)
-
-(when (require 'hlinum nil t)
-  (hlinum-activate))
+(provide 'init)
+;;; init.el ends here
