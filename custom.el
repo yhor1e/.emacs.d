73c40d470cbd2e8d9387e2d2635c5d02fa5e7f70
yhorie <yuk.horie@gmail.com>
2017-09-13 11:47:09 +0900

    Polish

--- custom.el
+++ custom.el
@@ -1,14 +1,22 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-search-threshold 1000)
   '(custom-safe-themes
      (quote
-       ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
+       ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+  '(package-selected-packages
+     (quote
+       (undohist yasnippet web-mode volatile-highlights use-package smex smart-mode-line rjsx-mode redo+ popwin mozc markdown-mode json-mode init-loader indent-guide hlinum git-gutter flycheck exec-path-from-shell emmet-mode editorconfig direx auto-install auto-complete anzu))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(sml/filename ((t (:foreground "deep sky blue"))))
+ '(sml/global ((t (:foreground "dark gray"))))
+ '(sml/line-number ((t (:foreground "dark gray" :weight bold)))))
