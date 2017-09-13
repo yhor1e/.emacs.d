f1b34b91da84c1dad0445bf9c3362a0d956a98e2
yhorie <yuk.horie@gmail.com>
2017-09-13 09:09:28 +0900

    Add customize info to git

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(anzu-deactivate-region t)
+ '(anzu-mode-lighter "")
+ '(anzu-search-threshold 1000)
+  '(custom-safe-themes
+     (quote
+       ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+  '(package-selected-packages
+     (quote
+       (solarized-theme flycheck-color-mode-line smart-mode-line indent-guide markdown-mode json-mode rjsx-mode js2-mode web-mode yasnippet volatile-highlights smex git-gutter flycheck exec-path-from-shell emmet-mode editorconfig direx auto-install auto-complete anzu popwin redo+ hlinum mozc init-loader use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(sml/filename ((t (:inherit sml/global :foreground "deep sky blue" :weight bold)))))
