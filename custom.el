d8de66ec38dbd3414b4183ea0cb20b2cf162be73
yhorie <horie.yuuki@nttcom.co.jp>
2017-09-13 09:24:36 +0900

    Polish

--- custom.el
+++ custom.el
@@ -6,15 +6,14 @@
  '(anzu-deactivate-region t)
  '(anzu-mode-lighter "")
  '(anzu-search-threshold 1000)
-  '(custom-safe-themes
-     (quote
-       ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
-  '(package-selected-packages
-     (quote
-       (solarized-theme flycheck-color-mode-line smart-mode-line indent-guide markdown-mode json-mode rjsx-mode js2-mode web-mode yasnippet volatile-highlights smex git-gutter flycheck exec-path-from-shell emmet-mode editorconfig direx auto-install auto-complete anzu popwin redo+ hlinum mozc init-loader use-package))))
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (solarized-theme flycheck-color-mode-line smart-mode-line indent-guide markdown-mode json-mode rjsx-mode js2-mode web-mode yasnippet volatile-highlights smex git-gutter flycheck exec-path-from-shell emmet-mode editorconfig direx auto-install auto-complete anzu popwin redo+ hlinum mozc init-loader use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(sml/filename ((t (:inherit sml/global :foreground "deep sky blue" :weight bold)))))
