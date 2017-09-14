69f278aa41095fdd7639c265c45b00d49b2cbd1d
yhorie <yuk.horie@gmail.com>
2017-09-14 10:34:31 +0900

    Add direx-k.el

--- inits/10_direx.el
+++ inits/10_direx.el
@@ -3,4 +3,16 @@
   :config
   (push '(direx:direx-mode :position left :width 50 :dedicated t)
     popwin:special-display-config)
-  (global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window))
+;  (global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
+
+  (global-set-key (kbd "C-x C-j")
+    (lambda () (interactive)
+      (direx:jump-to-directory-other-window)
+      (use-package dired-k
+        :ensure t
+        :config
+        (direx-k)
+      )
+      )
+    )
+  )
