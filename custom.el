2bda5adaf58ada762071b809a6d2209f71d0cfa7
yhorie <yuk.horie@gmail.com>
2018-02-18 23:15:48 +0900

    Add erlang-mode

--- custom.el
+++ custom.el
@@ -6,12 +6,12 @@
  '(anzu-deactivate-region t)
  '(anzu-mode-lighter "")
  '(anzu-search-threshold 1000)
-  '(custom-safe-themes
-     (quote
-       ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
-  '(package-selected-packages
-     (quote
-       (undohist yasnippet web-mode volatile-highlights use-package smex smart-mode-line rjsx-mode redo+ popwin mozc markdown-mode json-mode init-loader indent-guide hlinum git-gutter flycheck exec-path-from-shell emmet-mode editorconfig direx auto-install auto-complete anzu))))
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(package-selected-packages
+   (quote
+    (erlang undohist yasnippet web-mode volatile-highlights use-package smex smart-mode-line rjsx-mode redo+ popwin mozc markdown-mode json-mode init-loader indent-guide hlinum git-gutter flycheck exec-path-from-shell emmet-mode editorconfig direx auto-install auto-complete anzu))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
