c2faadf0635f505630334fbbab042d24e772a418
yhorie <yuk.horie@gmail.com>
2018-09-01 01:34:09 +0900

    Remove auto-install

--- inits/10_auto-install.el
+++
@@ -1,6 +0,0 @@
-(use-package auto-install
-  :ensure t
-  :config
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-  (auto-install-update-emacswiki-package-name t)
-  (auto-install-compatibility-setup))
