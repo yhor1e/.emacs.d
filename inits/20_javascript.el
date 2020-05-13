e7c0abf4b1f69c06d63d768286a34881f052cd7d
yhorie <yuk.horie@gmail.com>
2020-05-14 04:42:03 +0900

    ?

--- inits/20_javascript.el
+++ inits/20_javascript.el
@@ -9,8 +9,27 @@
 
 (use-package rjsx-mode
   :ensure t
-  :mode (("components\\/.*\\.js\\'" . rjsx-mode)
-          ("containers\\/.*\\.js\\'" . rjsx-mode))
+  ;:mode ("\\/[A-G].*\\.js\\'" . rjsx-mode)
+  ;:mode ("\\/[ABCDEFG].*\\.js\\'" . rjsx-mode)
+  ;:mode ("\\/[ABCDEFGHIJKLMN].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQRSTU].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQR].*\\.js\\'" . rjsx-mode)
+  ;OK:mode ("\\/[ABCDEFGHIJKLMNOP].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQ\\R].*\\.js\\'" . rjsx-mode)
+  ;OK:mode ("\\/[ABCDEFGHIJKLMNOPQ].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQS].*\\.js\\'" . rjsx-mode)
+  ;OK:mode ("\\/[ABCDEFGHIJKLMNOPQT].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQTUV].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQTU].*\\.js\\'" . rjsx-mode)
+  ;OK:mode ("\\/[ABCDEFGHIJKLMNOPQTV].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQTVWXY].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQTVW].*\\.js\\'" . rjsx-mode)
+  ;OK:mode ("\\/[ABCDEFGHIJKLMNOPQTVX].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQTVXY].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQTVXZ].*\\.js\\'" . rjsx-mode)
+  ;NG:mode ("\\/[ABCDEFGHIJKLMNOPQTVX]?Z?.*\\.js\\'" . rjsx-mode)
+  ;NG RSUWYZ
+  :mode ("\\/[ABCDEFGHIJKLMNOPQTVX].*\\.js\\'" . rjsx-mode)
   )
 
 (use-package eglot
