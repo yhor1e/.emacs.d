35884f18311dec147e0fb52584ccf08ac77d22f2
yhorie <yuk.horie@gmail.com>
2020-12-22 23:42:15 +0900

    Polish spaces and tab setting

--- inits/01_space-and-tab.el
+++ inits/01_space-and-tab.el
@@ -1,11 +1,8 @@
-;;; spaces, tabs
-;; disable whitespace
 (global-whitespace-mode 0)
+
 ;; trailing space
 (setq-default show-trailing-whitespace 0)
 (set-face-background 'trailing-whitespace "#b14770")
+
 ;; tab
-(use-package whitespace
-  :config
-  (setq whitespace-style '(tabs tab-mark))
-  (global-whitespace-mode 1))
+(setq whitespace-style '(tabs tab-mark))
