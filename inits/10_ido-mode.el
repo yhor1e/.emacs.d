37cfd6c2fbbe7dc4eeda2de84b7dd08f5744488d
yhorie <yuk.horie@gmail.com>
2020-12-24 00:09:46 +0900

    Update ido-mode.el

--- inits/10_ido-mode.el
+++ inits/10_ido-mode.el
@@ -1,10 +1,3 @@
-;;; ido-mode
-(ido-mode t)
-(use-package ido
-  :config
-  (setq ido-everywhere t)
-  (setq ido-enable-flex-matching t)
-  (setq ido-create-new-buffer 'always))
-(when (boundp 'confirm-nonexistent-file-or-buffer)
-  (setq confirm-nonexistent-file-or-buffer nil))
-(global-set-key (kbd "C-x f") 'ido-find-file-other-window)
+(ido-mode 1)
+(ido-everywhere 1)
+(setq ido-enable-flex-matching t)
