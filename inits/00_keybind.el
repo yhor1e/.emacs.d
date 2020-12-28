70f93956952696ee37154f2f689804024caba667
yhorie <yuk.horie@gmail.com>
2020-12-29 01:07:45 +0900

    Polish which-key

--- inits/00_keybind.el
+++ inits/00_keybind.el
@@ -1,4 +1,4 @@
-(keyboard-translate ?\C-h ?\C-?) ;; C-h
-(global-set-key (kbd "C-x ?") 'help-command) ;; C-x ?
-(global-set-key "\C-z" 'undo) ;; C-z
-(define-key global-map (kbd "C-t") 'other-window) ;; C-t
+(global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "C-x ?") 'help-command)
+(global-set-key (kbd "C-z") 'undo)
+(define-key global-map (kbd "C-t") 'other-window)
--- inits/10_which-key.el
+++ inits/10_which-key.el
@@ -1,7 +1,7 @@
 (use-package which-key
-  :ensure t
-  :diminish which-key-mode
-  :hook (after-init . which-key-mode)
-  :config
-  (which-key-setup-side-window-bottom)
-  )
+    :init (which-key-mode)
+    :diminish which-key-mode
+    :config
+    (setq which-key-show-major-mode t)
+    (setq which-key-show-early-on-C-h t)
+    (setq which-key-idle-delay 0.5))
