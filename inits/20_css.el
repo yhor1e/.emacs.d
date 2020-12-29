a68928ddd904b521f913ae052493768bc384f95d
yhorie <yuk.horie@gmail.com>
2020-12-29 22:52:39 +0900

    Polish css mode

--- inits/20_css.el
+++ inits/20_css.el
@@ -1,3 +1,7 @@
 (use-package css-mode
+  :functions flycheck-add-mode
+  :hook ((css-mode . add-node-modules-path)
+         (-mode . prettier-js-mode))
   :mode (("\\.css$" . css-mode))
-  )
+  :config
+  (flycheck-add-mode 'css-stylelint 'css-mode))
