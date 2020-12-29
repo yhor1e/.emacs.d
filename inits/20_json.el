f6eb9f7f26cb20192d7dfd0eadd71c5a6f52120a
yhorie <yuk.horie@gmail.com>
2020-12-29 23:20:57 +0900

    Polish json mode

--- inits/20_json.el
+++ inits/20_json.el
@@ -1,3 +1,5 @@
 (use-package json-mode
   :ensure t
-  :mode ("\\.json?$" . json-mode))
+  :mode ("\\.json?$" . json-mode)
+  :hook ((json-mode . add-node-modules-path)
+         (json-mode . prettier-js-mode)))
