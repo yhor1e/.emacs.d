410b12ed095592cbc4f020fc5d55fe3d692560e9
yhorie <yuk.horie@gmail.com>
2020-12-29 12:51:50 +0900

    Polish paren mode

--- inits/10_paran-mode.el
+++ inits/10_paran-mode.el
@@ -1,7 +1,6 @@
-(setq show-paren-delay 0)
 (show-paren-mode t)
+(setq show-paren-delay 0)
 (setq show-paren-style 'expression)
-;(set-face-background 'show-paren-match-face )
 (set-face-attribute 'show-paren-match nil
                     :background "#424242"
                     )
