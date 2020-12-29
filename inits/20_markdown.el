c11dbbfb0a132f734797071e3c66e0e7262bfc9b
yhorie <yuk.horie@gmail.com>
2020-12-29 22:30:36 +0900

    Polish markdown

--- inits/20_markdown.el
+++ inits/20_markdown.el
@@ -1,9 +1,12 @@
 (use-package markdown-mode
   :ensure t
+  :functions flycheck-add-mode
   :commands (markdown-mode gfm-mode)
   :hook ((markdown-mode . add-node-modules-path)
           (markdown-mode . prettier-js-mode))
   :mode (("README\\.md\\'" . gfm-mode)
           ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "marked"))
+          ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "marked")
+  :config
+  (flycheck-add-mode 'textlint 'markdown-mode))
