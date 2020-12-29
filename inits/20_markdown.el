510f7d387bbb12c182567b94a91ec89b04454f54
yhorie <yuk.horie@gmail.com>
2020-12-29 22:13:48 +0900

    Polish markdown

--- inits/20_markdown.el
+++ inits/20_markdown.el
@@ -1,6 +1,8 @@
 (use-package markdown-mode
   :ensure t
   :commands (markdown-mode gfm-mode)
+  :hook ((markdown-mode . add-node-modules-path)
+          (markdown-mode . prettier-js-mode))
   :mode (("README\\.md\\'" . gfm-mode)
           ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
